a98b1b3f0c687274996d274f7a26554610d76f64
taehyungz <thkthk97@naver.com>
2022-06-06 18:25:10 +0900

    refactor: sum 코드 정리

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,11 +5,19 @@ public class StringCalculator {
         if (input == null || input.isEmpty()) {
             return 0;
         }
-        String[] splittedInput = input.split(",");
+        String[] splittedInput = getSplittedInput(input);
+        return getSum(splittedInput);
+    }
+
+    private int getSum(String[] splittedInput) {
         int sum = 0;
         for (String number : splittedInput) {
             sum += Integer.parseInt(number);
         }
         return sum;
+    }
+
+    private String[] getSplittedInput(String input) {
+        return input.split(",");
     }
 }
